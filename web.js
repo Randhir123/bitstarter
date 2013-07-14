6ba6bd373ffea4424f80f1c12e27bc4b15dc9fb3
Randhir Singh <randhirkumars@yahoo.com>
2013-07-14 03:32:14 +0000

    bug fixes

--- web.js
+++ web.js
@@ -5,11 +5,13 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-   fs.readFileSync('./index.html', 'utf8', function(err, data) {
-       if (err) throw err;
-       var buffer = new Buffer('Hello world from index.html');
-       response.send(buffer.toString('utf8'));
-   });
+   var buffer = new Buffer('Hello world from index.html');
+   response.send(buffer.toString('utf8'));
+  // fs.readFileSync('./index.html', 'utf8', function(err, data) {
+  //     if (err) throw err;
+  //     var buffer = new Buffer('Hello world from index.html');
+  //     response.send(buffer.toString('utf8'));
+  // });
 });
 
 var port = process.env.PORT || 5000;
